e9e5b53ca957e446ebc023d114a707ce27b634af
Jakub Kubasiak <kubakubasiak@o2.pl>
2020-11-08 20:47:07 +0100

    added file browser

---
+++ Cube.Secure.WinForms/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Cube.Secure.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.fileImageList = new System.Windows.Forms.ImageList(this.components);
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.SelectedItems = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ItemNames = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(688, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(688, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Encrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(688, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Decrypt";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.LargeImageList = this.fileImageList;
+            this.listView.Location = new System.Drawing.Point(13, 74);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(669, 484);
+            this.listView.TabIndex = 3;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.Click += new System.EventHandler(this.listView_Click);
+            this.listView.DoubleClick += new System.EventHandler(this.listView_DoubleClick);
+            // 
+            // fileImageList
+            // 
+            this.fileImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.fileImageList.ImageSize = new System.Drawing.Size(32, 32);
+            this.fileImageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SelectedItems,
+            this.ItemNames});
+            this.statusStrip.Location = new System.Drawing.Point(0, 578);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip.TabIndex = 4;
+            // 
+            // SelectedItems
+            // 
+            this.SelectedItems.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.SelectedItems.Name = "SelectedItems";
+            this.SelectedItems.Size = new System.Drawing.Size(4, 17);
+            // 
+            // ItemNames
+            // 
+            this.ItemNames.Name = "ItemNames";
+            this.ItemNames.Size = new System.Drawing.Size(0, 17);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Enabled = false;
+            this.pathTextBox.Location = new System.Drawing.Point(13, 43);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(669, 23);
+            this.pathTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ImageList fileImageList;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.ToolStripStatusLabel SelectedItems;
+        private System.Windows.Forms.ToolStripStatusLabel ItemNames;
+    }
+}
+
---
+++ Cube.Secure.WinForms/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace Cube.Secure.WinForms
+{
+    public partial class Form1 : Form
+    {
+        string currentDirectory = string.Empty;
+        List<string> paths = new List<string>();
+        List<string> selectedPaths = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Select your path";
+                if(folderBrowserDialog.ShowDialog(this) == DialogResult.OK)
+                {
+                    RefreashFileList(folderBrowserDialog.SelectedPath);
+                }
+
+            }
+        }
+
+        private void RefreashFileList(string path)
+        {
+            this.currentDirectory = path;
+            this.pathTextBox.Text = path;
+            this.fileImageList.Images.Clear();
+            this.listView.Items.Clear();
+            this.paths.Clear();
+
+            this.fileImageList.Images.Add(new Icon(Path.Combine(Environment.CurrentDirectory, @"Assets\", "back.ico")));
+            DirectoryInfo currentDirectoryInfo = Directory.GetParent(this.currentDirectory);
+            this.paths.Add(currentDirectoryInfo.FullName);
+            this.listView.Items.Add(currentDirectoryInfo.Name, 0);
+
+            var directories = Directory.GetDirectories(this.currentDirectory);
+            for (int i = 0; i < directories.Length; i++)
+            {
+                this.fileImageList.Images.Add(new Icon(Path.Combine(Environment.CurrentDirectory, @"Assets\", "blank.ico")));
+                DirectoryInfo directoryInfo = new DirectoryInfo(directories[i]);
+                this.paths.Add(directoryInfo.FullName);
+                this.listView.Items.Add(directoryInfo.Name, 1);
+            }
+
+            var files = Directory.GetFiles(this.currentDirectory);
+            for (int i = 0; i < files.Length; i++)
+            {
+                this.fileImageList.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(files[i]));
+                DirectoryInfo directoryInfo = new DirectoryInfo(files[i]);
+                this.paths.Add(directoryInfo.FullName);
+                this.listView.Items.Add(directoryInfo.Name, this.fileImageList.Images.Count - 1);
+            }
+        }
+
+        private void listView_DoubleClick(object sender, EventArgs e)
+        {
+            var selectedPath = this.paths[this.listView.SelectedIndices[0]];
+            if (Directory.Exists(selectedPath))
+            {
+                this.RefreashFileList(selectedPath);
+            }
+        }
+
+        private void listView_Click(object sender, EventArgs e)
+        {
+            var names = new List<string>();
+            foreach (int index in this.listView.SelectedIndices)
+            {
+                var fullPath = this.paths[index];
+                var name = Path.GetFileName(fullPath);
+                names.Add(name);
+                //names.Add(fullPath);
+                this.selectedPaths.Add(fullPath);
+            }
+
+            this.ItemNames.Text = $"Names: {string.Join(", ", names)}";
+            this.SelectedItems.Text = $"Selected: {this.listView.SelectedIndices.Count}";
+        }
+    }
+}
