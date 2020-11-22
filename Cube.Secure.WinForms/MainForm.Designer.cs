91af262568eb38f887ca69e14f97e12d8ff4a80f
Jakub Kubasiak <kubakubasiak@o2.pl>
2020-11-22 17:55:36 +0100

    ui improvement

--- Cube.Secure.WinForms/MainForm.Designer.cs
+++ Cube.Secure.WinForms/MainForm.Designer.cs
@@ -94,7 +94,6 @@ namespace Cube.Secure.WinForms
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Location = new System.Drawing.Point(13, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 25);
@@ -125,6 +124,9 @@ namespace Cube.Secure.WinForms
             // 
             // listView
             // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.listView.HideSelection = false;
             this.listView.LargeImageList = this.fileImageList;
             this.listView.Location = new System.Drawing.Point(13, 80);
@@ -562,7 +564,7 @@ namespace Cube.Secure.WinForms
             this.Controls.Add(this.listView);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip;
-            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.MinimumSize = new System.Drawing.Size(700, 600);
             this.Name = "MainForm";
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
